9b92efe2dccbc0a39ae01a7ea2d522859ddc0578
Aayush_Neupane <neupane0403@gmail.com>
2020-09-06 17:04:14 +0545

    help updated

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,16 @@
+description = {
+  summary: "Shows summary from Nepal stock exchange website",
+  news: "scrap top 5 news from sharesansar and disply",
+  topgainers: "shows top gainers from nepalstock exchange site",
+};
+
 module.exports = {
   name: "help",
   execute: (message) => {
-    message.channel.send(`\`\`\`helped\`\`\``);
+    let displayMessage = "*****You can Use following commands****\n";
+    for (i in description) {
+      displayMessage += "! " + i + ": " + description[i] + "\n";
+    }
+    message.channel.send(`\`\`\`${displayMessage}\`\`\``);
   },
 };
